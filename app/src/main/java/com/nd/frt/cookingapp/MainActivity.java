7ea008bae3214cadd7ee469030239262714e710b
shiliqing123 <3537326885@qq.com>
2019-08-02 16:53:05 +0800

    cooking

--- app/src/main/java/com/nd/frt/cookingapp/MainActivity.java
+++ app/src/main/java/com/nd/frt/cookingapp/MainActivity.java
@@ -1,41 +1,64 @@
 package com.nd.frt.cookingapp;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
+import android.app.Activity;import android.os.Bundle;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.PopupMenu.OnMenuItemClickListener;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends Activity implements OnClickListener,OnMenuItemClickListener{
+
+
+    private Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActionBar supportActionBar = getSupportActionBar();
-        supportActionBar.setDisplayHomeAsUpEnabled(true);
-        supportActionBar.setHomeAsUpIndicator(R.mipmap.ic_launcher);
+        button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        PopupMenu popup = new PopupMenu(this, v);
+
+        MenuInflater inflater = popup.getMenuInflater();
+
+        inflater.inflate(R.menu.home, popup.getMenu());
+
+        popup.setOnMenuItemClickListener(this);
+
+        popup.show();
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.home,menu);
-        return true;
-    }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        switch (item.getItemId())
-        {
-            case R.id.newAc:
-                startActivity(new Intent(MainActivity.this,Main2Activity.class));
+    public boolean onMenuItemClick(MenuItem item) {
+        // TODO Auto-generated method stub
+        switch (item.getItemId()) {
+            case R.id.exit:
+                Toast.makeText(this, "退出", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.set:
+                Toast.makeText(this, "设置", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.account:
+                Toast.makeText(this, "账号", Toast.LENGTH_SHORT).show();
+                break;
+            default:
                 break;
         }
-
         return false;
     }
+
 }
+
+
